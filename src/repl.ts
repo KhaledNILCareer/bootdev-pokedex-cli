0cb90e65a721b9a5b6fa65d855459ca912b88448
KhaledNILCareer <KhaledNILCareer@gmail.com>
2026-09-15 17:57:38 +0300

    feat: implement interactive REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,20 @@
+import { createInterface } from "node:readline";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (i) => {
+    if(i.length > 0){
+      console.log(`Your command was: ${cleanInput(i)[0]}`)
+    }
+    rl.prompt();
+  });
+}
+
 export function cleanInput(input: string): string[] {
 
   return input.trim().toLowerCase().split(/\s+/)
