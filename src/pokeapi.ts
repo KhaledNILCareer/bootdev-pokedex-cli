acb5e5e325c2b4ef1a6bbfa694dd80f78525f6e4
KhaledNILCareer <KhaledNILCareer@gmail.com>
2026-09-17 16:40:05 +0300

    feat: add Pokemon inspect command

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -93,5 +93,18 @@ export type Location = {
 
 export type Pokemon = {
   "name": string,
-  base_experience: number
+  base_experience: number,
+  "height": number,
+  "weight": number,
+  "stats": {
+    "base_stat": number,
+    "stat": {
+      "name": string
+    }
+  }[],
+  types : {
+    "type": {
+    "name": string
+    }
+  }[]
 };
